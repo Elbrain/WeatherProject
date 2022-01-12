fe8e189c372bfe52bd690d604901480e232ad3f6
Alex <alexdolzenko@gmail.com>
2022-01-12 13:23:47 +0000

    Initial commit

---
+++ app/src/main/java/uk/org/websolution/weatherproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package uk.org.websolution.weatherproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uk.org.websolution.weatherproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
